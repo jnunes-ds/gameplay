0b1bd44a964d88a564f9b9508a3038562fa312a0
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-21 02:55:02 -0300

    Finishe SingIn background

--- src/screens/SingIn/index.tsx
+++ src/screens/SingIn/index.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import {
     View,
     Text,
-    Image 
+    Image,
+    StatusBar 
 } from 'react-native';
 
 import IllustrationImg from '../../assets/illustration.png'
@@ -15,7 +16,27 @@ export function SingIN(){
 
     return (
         <View style={styles.container} >
-            <Image source={IllustrationImg} />
+            <StatusBar 
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            <Image 
+                source={IllustrationImg}
+                style={styles.image} 
+            />
+            <View style={styles.content} >
+                <Text style={styles.title} >
+                    Organize {`\n`}
+                    suas jogatinas {`\n`}
+                    facilmente
+                </Text>
+
+                <Text style={styles.subtitle} >
+                    Crie grupos para jogar games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+            </View>
         </View>
     );
 }
