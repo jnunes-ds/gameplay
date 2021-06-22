15b889289da1c67fa5da57ccb74e7b8657360cd3
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-22 00:55:31 -0300

    Profile component created

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+import { Avatar } from '../Avatar';
+
+export function Profile(){
+    return (
+        <View style={styles.container} >
+
+            <Avatar urlImage="https://github.com/jnunes-ds.png" />
+
+            <View>
+                <View style={styles.user} >
+                    <Text style={styles.greeting} >
+                        Olá,
+                    </Text>
+                    <Text style={styles.username}>
+                        Júnior
+                    </Text>
+                </View>
+                    <Text style={styles.message}>
+                        Hoje é dia de vitória
+                    </Text>
+            </View>
+        </View>
+    );
+}
