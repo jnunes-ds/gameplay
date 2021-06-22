42642e1150e4443600163fab9762d7644e2a9d3f
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-22 20:40:45 -0300

    Category card adjusted

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -11,15 +11,17 @@ interface Props extends RectButtonProps{
     title: string;
     icon: React.FC<SvgProps>;
     checked?: boolean;
+    hasCheckBox?: boolean;
 }
 
 export function Category({
     title,
     icon: Icon,
     checked = false,
+    hasCheckBox = false,
     ...rest
 } : Props){
-    const { secondary50, secondary70 } = theme.colors;
+    const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
 
     return (
         <RectButton
@@ -32,13 +34,20 @@ export function Category({
                     style={styles.container}
                     colors={[secondary50, secondary70]}
                 >
-                    <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-                        <View style={
-                            checked 
-                            ? styles.checked 
-                            : styles.check
-                            }>
-                        </View>
+                    <LinearGradient 
+                        style={[styles.content, 
+                        { opacity: checked ? 1 : 0.4 }]}
+                        colors={[ checked ? secondary85 : secondary50, secondary40 ]}
+                    >
+                        {
+                            hasCheckBox &&
+                            <View style={
+                                checked 
+                                ? styles.checked 
+                                : styles.check
+                                }>
+                            </View>
+                        }
                             <Icon 
                                 width={48}
                                 height={48}
@@ -47,7 +56,7 @@ export function Category({
                         <Text style={styles.title}>
                             { title }
                         </Text>
-                    </View>
+                    </LinearGradient>
                 </LinearGradient>
             </View>
         </RectButton>
