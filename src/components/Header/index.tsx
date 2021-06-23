faf379327e944f5b670f300ec27ca7427c8a744a
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-23 01:33:53 -0300

    handleGoBak function working

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,7 @@ import { Text, View } from 'react-native';
 
 import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
+import { useNavigation } from '@react-navigation/native';
 
 interface Props{
     title: string;
@@ -13,18 +14,25 @@ interface Props{
 }
 
 export function Header({ title, action } : Props){
+    const navigation = useNavigation();
     const {
         secondary100,
         secondary40,
         heading 
     } = theme.colors;
 
+    function handleGoBack(){
+        navigation.goBack();
+    }
+
     return (
         <LinearGradient
             style={styles.container}
             colors={[secondary100, secondary40]}
         >
-            <BorderlessButton>
+            <BorderlessButton
+                onPress={handleGoBack}
+            >
                 <Feather 
                     name="arrow-left"
                     size={24}
