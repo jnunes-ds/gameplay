663c568cfb770d18d584b26acd5c0573eea23bb6
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-24 16:56:10 -0300

    Authentication working

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -47,7 +47,7 @@ function AuthProvider({ children } : AuthProviderProps){
             const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
             
 
-            const response = AuthSession.startAsync({ authUrl });
+            const response = await AuthSession.startAsync({ authUrl });
             console.log(response);
 
         } catch {
